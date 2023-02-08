8d8dfb3c14377e079eed39218c44bc5022919305
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-08 18:41:38 +0100

    created route comic

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name(('home'));
+
+
+Route::get('/', function () {
+    $comic = config('comics.comic');
+    return view('comic');
+})->name(('comic'));
