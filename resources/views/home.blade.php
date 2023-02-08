9c95396dfe366e06b14a656dfb02a213691713ce
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-08 17:44:06 +0100

    added jumbotron

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+@section ('main-content')
+
+<div class="jumbo-container">
+    <img  src="{{Vite::asset('resources/assets/img/jumbotron.jpg')}}" alt="jumbo image">
+</div>
+
+@endsection
